cad7aa7d99bb18667f5c6dabfc2f435c98736297
Tsur <tsur2912@gmail.com>
2018-04-03 19:00:23 +0300

    Nice

--- app/src/main/java/com/marx/amit/gamerground/DAO/GameSellDAO.java
+++ app/src/main/java/com/marx/amit/gamerground/DAO/GameSellDAO.java
@@ -1,5 +1,9 @@
 package com.marx.amit.gamerground.DAO;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.marx.amit.gamerground.model.GameSell;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -8,7 +12,30 @@ import java.util.Date;
  */
 
 public class GameSellDAO {
+    private static DatabaseReference mDatabase;
+    private static GameSellDAO sharedInstance = null;
+    private static DatabaseReference gameSellRef = null;
 
+    private GameSellDAO() {
+        this.mDatabase = FirebaseDatabase.getInstance().getReference();
+        this.gameSellRef = mDatabase.getDatabase().getReference("gameSell");
+    }
+
+    public static GameSellDAO getInstance() {
+        if (sharedInstance == null) {
+            sharedInstance = new GameSellDAO();
+        }
+        return sharedInstance;
+    }
+
+    public static void writeGameSell(GameSell gameSell) {
+        getInstance();
+        gameSellRef.child(gameSell.getSellId()).setValue(gameSell);
+    }
+
+    public static void deleteGameSell(GameSell gameSell) {
+        gameSellRef.child(gameSell.getSellId()).setValue(null);
+    }
 
     public static String getTimeStamp() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
